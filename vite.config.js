1817c591144903f6b5982fcfe13ed4ae854bc56c
付俊伟 <fujunwei@yadingdata.com>
2025-08-10 16:57:50 +0800

    11

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import pxtorem from 'postcss-pxtorem';
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
   ],
